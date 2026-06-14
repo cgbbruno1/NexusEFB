0f37372e7125ac861c9c34a46c688ff211f2c438
cgbbruno1 <92726925+cgbbruno1@users.noreply.github.com>
2026-06-14 09:57:38 -0400

    chore: add modular foundation baseline

---
+++ lib/features/logbook/data/logbook_repository_provider.dart
@@ -0,0 +1,8 @@
+import 'package:avaremp/features/logbook/domain/logbook_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final logbookRepositoryProvider = Provider<LogbookRepository>((ref) {
+  throw UnimplementedError(
+    'logbookRepositoryProvider will be wired in a future sprint.',
+  );
+});
