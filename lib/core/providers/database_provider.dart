958ea93338ebc929ca0c854ba1f2c63dcf799986
cgbbruno1 <92726925+cgbbruno1@users.noreply.github.com>
2026-06-14 10:03:23 -0400

    chore: add modular foundation baseline

--- lib/core/providers/database_provider.dart
+++ lib/core/providers/database_provider.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final appDatabaseProvider = Provider<AppDatabase>((ref) {
   final database = AppDatabase();
-  ref.onDispose(database.close);
+  ref.onDispose(() {
+    database.close();
+  });
   return database;
 });
