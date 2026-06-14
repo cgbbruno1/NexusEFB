62ab9eac08439b299011d2d77f8131bca3ed0d97
cgbbruno1 <92726925+cgbbruno1@users.noreply.github.com>
2026-06-14 09:56:47 -0400

    chore: add modular foundation baseline

---
+++ lib/features/aircraft/data/aircraft_repository_provider.dart
@@ -0,0 +1,8 @@
+import 'package:avaremp/features/aircraft/domain/aircraft_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final aircraftRepositoryProvider = Provider<AircraftRepository>((ref) {
+  throw UnimplementedError(
+    'aircraftRepositoryProvider will be wired in a future sprint.',
+  );
+});
